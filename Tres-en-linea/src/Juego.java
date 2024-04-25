a37700a30d9e5684e9f40bea24011042eefdf69b
CrisSSD <cdcalderonrojas@gmail.com>
2024-04-25 19:52:01 +0200

    Clase juego: se crearon nuevos metodos

--- Tres-en-linea/src/Juego.java
+++ Tres-en-linea/src/Juego.java
@@ -1,3 +1,15 @@
 public class Juego {
 
+    public void nuevaPartida(){
+
+    }
+
+    public void jugar(short fila, String columna){
+
+    }
+
+    public boolean jugadaGanadora(short fila, short columna){
+        return false;
+    }
+
 }
