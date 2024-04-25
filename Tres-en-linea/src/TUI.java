7b8297443e0234fb9938240ab394c6e32cce67da
portatil Ctapasco <ctapasco@elpuig.xeill.net>
2024-04-25 19:45:57 +0200

    clase TIU : se crearon nuevos metodo

--- Tres-en-linea/src/TUI.java
+++ Tres-en-linea/src/TUI.java
@@ -1,3 +1,21 @@
 public class TUI {
 
+
+    public int mostrarMenu() {
+
+        return 0;
+    }
+
+    public void mostrarTablero(char tablero[], short turno) {
+
+    }
+
+    public short[] recogerJugada() {
+        short[] ejemplo = new short[1];
+        return ejemplo;
+    }
+
+    public void finDePartida(short ganador) {
+
+    }
 }
