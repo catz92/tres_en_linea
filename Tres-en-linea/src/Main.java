e975f9c8fe583545f622a235dd0aba1328081722
CrisSSD <cdcalderonrojas@gmail.com>
2024-04-25 20:43:25 +0200

    Opciones de menu de TUI

--- Tres-en-linea/src/Main.java
+++ Tres-en-linea/src/Main.java
@@ -1,53 +1,38 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        showMenu();
+        TUI tui = new TUI();
+        Juego joc = new Juego();
+        int resultado = tui.mostrarMenu();
+        System.out.print(resultado);
+        switch (resultado){
+            case 1:
+                nuevapartida();
+                break;
+            case 2:
+                cargapartida();
+                break;
+            case 3:
+                configuracion();
+                break;
+            case 4:
+                salir();
+                break;
+        }
     }
-    public  static void showMenu(){
-        System.out.println("Bienvenidos a Tres en linea");
-        System.out.println("Selecciona la opcion deseada");
-
-        int opcion = 0;
-        do{
-            System.out.println("1. Nueva partida");
-            System.out.println("2. Cargar partida");
-            System.out.println("3. Configuracion");
-            System.out.println("4. Salir");
-
-            Scanner sc = new Scanner(System.in);
-            opcion = sc.nextInt();
-
-            switch (opcion){
-                case 1:
-                    nuevapartida();
-                    break;
-                case 2:
-                    cargapartida();
-                    break;
-                case 3:
-                   configuracion();
-                    break;
-                case 4:
-                    salir();
-                    break;
-                default:
-                    System.out.println("Elija una opcion correcta");
-            }
-        }while (opcion!=4);
+    private static void nuevapartida() {
+        System.out.println("llegamos a nueva partida");
     }
 
     private static void cargapartida() {
-    }
-
-    private static void nuevapartida() {
-
-    }
-
-    private static void salir() {
+        System.out.println("llegamos a CARGAR PARTIDA");
 
     }
 
     private static void configuracion() {
+        System.out.println("llegamos a CONFIGURACION");
+
+    }
+    private static void salir() {
+        System.out.println("llegamos a SALIR");
     }
 }
--- Tres-en-linea/src/TUI.java
+++ Tres-en-linea/src/TUI.java
@@ -1,9 +1,33 @@
+import java.util.Scanner;
+
 public class TUI {
+    Scanner sc = new Scanner(System.in);
 
 
     public int mostrarMenu() {
+        System.out.println("Bienvenidos a Tres en linea");
+        System.out.println("Selecciona la opcion deseada");
+        int opcion = 0;
+        do {
+            System.out.println("1. Nueva partida");
+            System.out.println("2. Cargar partida");
+            System.out.println("3. Configuracion");
+            System.out.println("4. Salir");
+            opcion = sc.nextInt();
 
-        return 0;
+            switch (opcion){
+                case 1:
+                    return opcion;
+                case 2:
+                    return opcion;
+                case 3:
+                    return opcion;
+                default:
+                    System.out.println("Elija una opcion correcta");
+                    break;
+            }
+        }while (opcion!=4);
+        return opcion;
     }
 
     public void mostrarTablero(char tablero[], short turno) {
