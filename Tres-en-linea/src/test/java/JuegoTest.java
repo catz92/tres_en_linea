d693104987262dd6193c57e587378fd85a0eaf07
CrisSSD <cdcalderonrojas@gmail.com>
2024-05-02 20:53:31 +0200

    Maven aplicado en el proyecto

---
+++ Tres-en-linea/src/test/java/JuegoTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class JuegoTest {
+
+    @Test
+    void nuevaPartida() {
+    }
+
+    @Test
+    void jugar() {
+    }
+
+    @Test
+    void jugadaGanadora() {
+    }
+}
