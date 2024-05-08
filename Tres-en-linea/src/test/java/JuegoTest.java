c32c60ba57314f6e24791dcdac7e416441fb5cf5
portatil Ctapasco <ctapasco@elpuig.xeill.net>
2024-05-08 17:42:08 +0200

    actulizacion por error de ramas

--- Tres-en-linea/src/test/java/JuegoTest.java
+++ Tres-en-linea/src/test/java/JuegoTest.java
@@ -1,6 +1,8 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class JuegoTest {
@@ -15,30 +17,32 @@ class JuegoTest {
     }
 
     @Test
-    void nuevaPartida() {
+    void nuevaPartida_taullel() {
         Juego jue = new Juego();
         jue.nuevaPartida();
+        char[][] tableroEsperado = new char[3][3];
+        Arrays.stream(tableroEsperado).forEach(a -> Arrays.fill(a, '-'));
+
+        // Comprueba si las matrices son iguales
+        assertArrayEquals(tableroEsperado, jue.getTabla());
+    }
+
+    @Test
+    void jugar() {
+        Juego jue = new Juego();
+        jue.nuevaPartida();
+        jue.getTurn();
+        jue.jugar(0, 0);
+
+//        Matriz creada para prueba
         char[][] tableroEsperado = new char[3][3];
         for (int i = 0; i < tableroEsperado.length; i++) {
             for (int j = 0; j < tableroEsperado.length; j++) {
                 tableroEsperado[i][j] = '-';
             }
         }
-
-        // Comprueba si las matrices son iguales
-        assertArrayEquals(tableroEsperado, jue.getTabla());
-//        char [][] matrizllena = {{'-','-','-'},{'-','-','-'},{'-','-','-'}};
-    }
-
-    @Test
-    void jugar() {
-    Juego jue = new Juego();
-//    int fila = 1 ;
-//    int columna = 2;
-//    jue.jugar(1,2);
-//
-////    Verificar
-//        assertEquals('0',jue.getTabla());
+        tableroEsperado[0][0] = '0';
+        assertArrayEquals(tableroEsperado,jue.getTabla());
     }
 
     @Test
